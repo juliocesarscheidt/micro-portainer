c196122d632faeba075bd0a146741ee37f1069c0
juliocesarscheidt <julio.scheidt96@gmail.com>
2023-01-10 11:36:27 -0300

    Minor changes

--- micro-portainer/docker-entrypoint.sh
+++ micro-portainer/docker-entrypoint.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 set -x
 
 # set a default version for usage of docker API
 export V_DOCKER_API=${V_DOCKER_API:-1.41}
 echo "Docker API Version :: ${V_DOCKER_API}"
+
+echo "$@"
 
 cat /etc/nginx/nginx.conf | envsubst \${V_DOCKER_API} | tee /etc/nginx/nginx.conf
 /docker-entrypoint.sh "$@"
