b2e556623dbd448e1eb7f3d5653dce5e968ff509
juliocesarscheidt <julio.scheidt96@gmail.com>
2023-01-18 14:35:52 -0300

    Change docker

--- micro-portainer/docker-entrypoint.sh
+++ micro-portainer/docker-entrypoint.sh
@@ -3,10 +3,13 @@
 set -x
 
 # set a default version for usage of docker API
-export V_DOCKER_API=${V_DOCKER_API:-1.41}
+export V_DOCKER_API="${V_DOCKER_API:-1.41}"
 echo "Docker API Version :: ${V_DOCKER_API}"
 
 echo "$@"
 
-cat /etc/nginx/nginx.conf | envsubst \${V_DOCKER_API} | tee /etc/nginx/nginx.conf
+cp /etc/nginx/nginx.conf /tmp/nginx.conf
+envsubst '$V_DOCKER_API' < /tmp/nginx.conf | tee /etc/nginx/nginx.conf
+rm -f /tmp/nginx.conf
+
 /docker-entrypoint.sh "$@"
